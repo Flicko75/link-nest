bef195a0488dfc17ec0b99a25f08b2db2594f5f7
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-09-10 21:58:28 +0530

    url redirect functionality

--- url-shortener-sb/src/main/java/com/url/shortener/service/UrlMappingService.java
+++ url-shortener-sb/src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -96,4 +96,21 @@ public class UrlMappingService {
         return clickEvents.stream()
                 .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
     }
+
+    // Returning the original url for the passed short url and update the click event
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping != null){
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            // Record click event
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+
+        return urlMapping;
+    }
 }
