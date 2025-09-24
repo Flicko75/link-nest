af3f09e50619ab6643d5a3f1ad68ef09127943d4
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-09-24 19:44:43 +0530

    Login page and token generation after authentication

---
+++ url-shortener-frontend/src/components/Login.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import TextField from './TextField';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../api/api';
+import toast from 'react-hot-toast';
+
+const Login = () => {
+    const navigate =useNavigate();
+    const [loader, setLoader] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        },
+        mode: "onTouched",
+    })
+
+    const loginHandler = async (data) => {
+        setLoader(true);
+        try {
+            const { data: response } = await api.post(
+                "/api/auth/public/login",
+                data
+            );
+            // Store token in local storage
+            console.log(response.token);
+            localStorage.setItem("JWT_TOKEN", JSON.stringify(response.token));
+            
+            reset();
+            navigate("/");
+            toast.success("Login Successfull");
+        }
+        catch(error){
+            console.log(error);
+            toast.error("Login Failed");
+        }
+        finally {
+            setLoader(false);
+        }
+    };
+
+    return (
+        <div
+            className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
+            <form onSubmit={handleSubmit(loginHandler)}
+                className="sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md">
+                <h1 className="text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl">
+                    Login Here
+                </h1>
+
+                <hr className='mt-2 mb-5 text-black' />
+
+                <div className='flex flex-col gap-3'>
+                    <TextField
+                        label="Username"
+                        required
+                        id="username"
+                        type="text"
+                        message="*Username is required"
+                        placeholder="Type Your Username"
+                        register={register}
+                        errors={errors}
+                    />
+
+                    <TextField
+                        label="Password"
+                        required
+                        id="password"
+                        type="password"
+                        message="*Password is required"
+                        placeholder="Type Your Password"
+                        register={register}
+                        min={6}
+                        errors={errors}
+                    />
+                </div>
+
+                <button
+                    type='submit'
+                    className='bg-customRed font-semibold text-white cursor-pointer bg-custom-gradient w-full py-2 hover:text-black-100 rounded-sm my-3'>
+                    Login
+                </button>
+
+                <p className='text-center text-sm text-slate-600 mt-6'>
+                    Don't have an account?
+                    <Link
+                        className='font-semibold underline'
+                        to="/register">
+                        <span className='text-btnColor'>Register</span>
+                    </Link>
+                </p>
+            </form>
+        </div>
+    )
+}
+
+export default Login
