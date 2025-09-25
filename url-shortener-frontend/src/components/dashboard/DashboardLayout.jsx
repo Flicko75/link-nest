09d1b2ad231b5227bbadbad686512bd54c4b8def
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-09-25 21:58:15 +0530

    create short URL functionality added to frontend

--- url-shortener-frontend/src/components/dashboard/DashboardLayout.jsx
+++ url-shortener-frontend/src/components/dashboard/DashboardLayout.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Graph from './Graph'
 import { dummyData } from '../../dummyData/data'
 import { useStoreContext } from '../../contextApi/ContextApi'
 import { useFetchTotalClicks } from '../../hooks/useQuery'
+import ShortenPopUp from './ShortenPopUp'
 
 const DashboardLayout = () => {
+  const refetch = false;
   const { token } = useStoreContext();
+  const [shortenPopUp, setShortenPopUp] = useState(false);
 
   // console.log(useFetchTotalClicks(
   //   token, onError
@@ -38,12 +41,21 @@ const DashboardLayout = () => {
             <Graph graphdata={totalClicks} />
           </div>
           <div className='py-5 sm:text-end text-center'>
-            <button className='bg-custom-gradient px-4 py-2 rounded-md text-white cursor-pointer'>
+            <button 
+              className='bg-custom-gradient px-4 py-2 rounded-md text-white cursor-pointer'
+              onClick={() => setShortenPopUp(true)}
+            >
               Create a New Short URL
             </button>
           </div>
         </div>
       )}
+
+      <ShortenPopUp
+        refetch={refetch}
+        open={shortenPopUp}
+        setOpen={setShortenPopUp}
+      />
     </div>
   )
 }
