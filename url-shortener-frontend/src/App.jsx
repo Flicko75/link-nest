641460dacef8ff36467e59a5c6e88f90ea421480
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-09-26 22:16:15 +0530

    managing subdomains and redirects

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,31 +1,14 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './components/LandingPage'
-import AboutPage from './components/AboutPage'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import RegisterPage from './components/RegisterPage'
-import { Toaster } from 'react-hot-toast'
-import LoginPage from './components/LoginPage'
-import DashboardLayout from './components/dashboard/DashboardLayout'
+import { BrowserRouter } from 'react-router-dom'
+import { getApps } from './utils/helper'
 
 function App() {
+  const CurrentApp = getApps();
 
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Toaster position='top-center'/>
-          <Routes>
-            <Route path='/' element={<LandingPage />}/>
-            <Route path='/about' element={<AboutPage />}/>
-            <Route path='/register' element={<RegisterPage />}/>
-            <Route path='/login' element={<LoginPage />}/>
-            <Route path='/dashboard' element={<DashboardLayout />}/>
-          </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <CurrentApp />
+    </BrowserRouter>
   )
 }
 
