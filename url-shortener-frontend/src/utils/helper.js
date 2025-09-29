7b6294dbca1311ff7ce005c19654cfe02da5b370
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-09-29 23:22:37 +0530

    changing the redirection logic to work with netlify

--- url-shortener-frontend/src/utils/helper.js
+++ url-shortener-frontend/src/utils/helper.js
@@ -1,19 +1,31 @@
+import AppRouter, { SubDomainRouter } from "../AppRouter";
 import { subDomainList } from "./constant";
 
+// export const getApps = () => {
+//     const subdomain = getSubDomain(window.location.hostname);
+
+//     const mainApp = subDomainList.find((app) => app.main);
+//     if (subdomain === "") return mainApp.app;
+
+//     const apps = subDomainList.find((app) => subdomain === app.subDomain);
+
+//     return apps ? apps.app : mainApp.app;
+// }
 export const getApps = () => {
-    const subdomain = getSubDomain(window.location.hostname);
+    const pathParts = window.location.pathname.split("/").filter(Boolean);
+    const firstSegment = pathParts[0]; // 0QZMZf5A
 
-    const mainApp = subDomainList.find((app) => app.main);
-    if (subdomain === "") return mainApp.app;
+    // If the first path segment is a short URL 
+    const isShortUrl = /^[a-zA-Z0-9]{8}$/.test(firstSegment); 
 
-    const apps = subDomainList.find((app) => subdomain === app.subDomain);
-
-    return apps ? apps.app : mainApp.app;
+    if (isShortUrl) return SubDomainRouter; // render ShortenUrlPage
+    return AppRouter; // default landing pages
 }
 
-export const getSubDomain = (location) => {
-    const locationParts = location.split(".");
-    const isLocalhost = locationParts.slice(-1)[0] === "localhost";
-    const sliceTill = isLocalhost ? -1 : -2;
-    return locationParts.slice(0, sliceTill).join("");
-}
+
+// export const getSubDomain = (location) => {
+//     const locationParts = location.split(".");
+//     const isLocalhost = locationParts.slice(-1)[0] === "localhost";
+//     const sliceTill = isLocalhost ? -1 : -2;
+//     return locationParts.slice(0, sliceTill).join("");
+// }
